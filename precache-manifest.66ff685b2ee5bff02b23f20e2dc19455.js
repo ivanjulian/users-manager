860c6be36499469c38ce138df0ac3a058fdbd7cf
IvanKhizhnyak <vanja.khizhnjak99@gmail.com>
2020-07-19 21:30:23 +0300

    Updates

---
+++ precache-manifest.66ff685b2ee5bff02b23f20e2dc19455.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12788facb5f366ff9d6a39fe5455fac2",
+    "url": "/users-manager/index.html"
+  },
+  {
+    "revision": "397a0d704badd1905c21",
+    "url": "/users-manager/static/css/main.b0f2da09.chunk.css"
+  },
+  {
+    "revision": "ff7da86075b68b5cabd2",
+    "url": "/users-manager/static/js/2.82e03d48.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/users-manager/static/js/2.82e03d48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "397a0d704badd1905c21",
+    "url": "/users-manager/static/js/main.b1be015c.chunk.js"
+  },
+  {
+    "revision": "c0d15eef74a8ed7e88fc",
+    "url": "/users-manager/static/js/runtime-main.f8dc3308.js"
+  }
+]);
--- src/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { getUsersReducer } from "./reducers/getUsersReducer";
-
-export const rootReducer = combineReducers(
-  {
-    users: getUsersReducer
-  }
-)
